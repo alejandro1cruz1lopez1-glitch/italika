510fdaf1883430cf6d81604a109c77ef37206033
Alejandro <alejandro1cruz1lopez1@gmail.com>
2026-04-28 00:30:44 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,1 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = ""; 
+$db   = "italika_db";
 
+$conexion = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+?>
