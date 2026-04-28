2ebd376c335b6de277702ae5c6470fbad6a6fe60
Alejandro <alejandro1cruz1lopez1@gmail.com>
2026-04-28 10:34:57 -0600

    Update validar.php

--- validar.php
+++ validar.php
@@ -3,7 +3,7 @@ $nombre_usuario = $_POST['nombre'];
 $n_control = $_POST['n_control'];
 $password = $_POST['password'];
 
-if ($n_control == $password) {
+if ($n_control == 24160584 && $password== 24160584tso) {
     header("Location: inventario.php");
     exit();
 } else {
